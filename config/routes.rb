d44f17ad9b418ebe0f6b71eff9dcb80800361bd6
Valeria Pineda <valerializette.pinedacruz@kedgebs.com>
2022-06-14 12:05:41 +0200

    trip

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   resources :stories
   resources :survival_articles
   resources :activities
+  resources :trips
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
