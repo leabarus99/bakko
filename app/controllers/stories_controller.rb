fb2c6decc731c30035569aabb3ae188933d4927d
Lina El amrani <linelamrani@gmail.com>
2022-06-15 14:31:13 +0200

    Add dotenv - Protect my secret data in .env file

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,2 +1,29 @@
 class StoriesController < ApplicationController
+  def index
+    @stories = Story.all
+  end
+
+  def show
+    @story = Story.find(params[:id])
+  end
+
+  def new
+    @story = Story.new
+  end
+
+  def create
+    @story = Story.new(story_params)
+    if @story.save
+      redirect_to story_path(@story)
+    else
+      render :new
+    end
+    authorize @story
+  end
+
+  private
+
+  def story_params
+    params.require(:budget).permit(:content, :photo, :title, :video, :location, :trip)
+  end
 end
