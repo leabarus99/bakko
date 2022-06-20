503a15555b76d374f442b376d0046b3a536028f5
Lea Barus <lea.barus31@gmail.com>
2022-06-20 17:42:45 +0200

    navbar border

--- app/javascript/plugins/listener.js
+++ app/javascript/plugins/listener.js
@@ -1,26 +1,26 @@
 // TODO: React to a click on the button!
 
-// const moveIndicatorPrice = (id) => {
-//   let dragValue;
+const moveIndicatorPrice = (id) => {
+  let dragValue;
 
-//   const priceIndicator = document.getElementById("price-indicator");
-//   priceIndicator.style.position = "absolute";
-//   priceIndicator.ontouchmove = function() {
-//     dragValue = priceIndicator;
-//   }
+  const priceIndicator = document.getElementById("price-indicator");
+  priceIndicator.style.position = "absolute";
+  priceIndicator.ontouchmove = function() {
+    dragValue = priceIndicator;
+  }
 
-//   document.ontouchmove = function(e) {
-//     const x = e.pageX;
-//     const y = e.pageY;
+  document.ontouchmove = function(e) {
+    const x = e.pageX;
+    const y = e.pageY;
 
-//     dragValue.style.left = x + "px";
-//     dragValue.style.top = y + "px";
-//   }
+    dragValue.style.left = x + "px";
+    dragValue.style.top = y + "px";
+  }
 
-//   document.ontouchend = function(e) {
-//     dragValue = null
-//   }
-// }
+  document.ontouchend = function(e) {
+    dragValue = null
+  }
+}
 
 
 
