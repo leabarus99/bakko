fdaad403dea390f96f1a6a8e2cd8f7f1f81e2053
Lea Barus <lea.barus31@gmail.com>
2022-06-21 14:10:35 +0200

    improve migrations likes

--- db/migrate/20220619194519_add_story_column_to_likes.rb
+++
@@ -1,5 +0,0 @@
-class AddStoryColumnToLikes < ActiveRecord::Migration[6.1]
-  def change
-    add_reference :likes, :story, null: false, foreign_key: true
-  end
-end
--- db/migrate/20220620085517_remove_likes_table.rb
+++
@@ -1,5 +0,0 @@
-class RemoveLikesTable < ActiveRecord::Migration[6.1]
-  def change
-    drop_table :likes
-  end
-end
