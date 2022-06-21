c3f69d233a70c31784ad290282a0f1923bec9703
Lea Barus <lea.barus31@gmail.com>
2022-06-21 17:50:59 +0200

    seeds and equipment show page

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many_attached :photo
   has_many :trips, dependent: :destroy
-  has_many :likes, dependent: :destroy
+  has_many :liikes, dependent: :destroy
   # def full_name
   #   slef.first_name.cap
   # end
